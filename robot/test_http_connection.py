3d3f31d31c63f36fb8d46c39c43e0f661fbd23f1
David <david.kaiqiang.wang@gmail.com>
2017-07-08 22:01:38 +0800

    add test for http connection module

---
+++ robot/test_http_connection.py
@@ -0,0 +1,24 @@
+from robot.http_connection import HttpConnection
+import pytest
+import json
+import logging
+
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+class TestHttpConnection:
+    @classmethod
+    def setup_class(cls):
+        with open("proxies.json", "r") as f:
+            cls.proxy_cfg = json.load(f)
+        cls.conn = HttpConnection(cls.proxy_cfg)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.conn.close()
+
+    @pytest.mark.asyncio
+    async def test_request(self):
+        resp = await self.conn.request("http://www.google.com")
+        self.conn.close()
+        print(resp)
