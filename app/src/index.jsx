581c1a3e8ee412a0a66ef2b9019a088432f0025a
David <david.kaiqiang.wang@gmail.com>
2017-07-08 13:57:48 +0800

    a simple clock

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,7 +3,42 @@ import ReactDOM from 'react-dom';
 
 console.log("Hello world");
 
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {now: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(()=>{this.tick(); }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timerId);
+    }
+
+    tick() {
+        this.setState({now: new Date()});
+    }
+
+    render() {
+        return <p>Welcome to my clock, the time now is <strong>{this.state.now.toString()}</strong></p>
+    }
+}
+
+function clicked_me() {
+    alert("you clicked the button!");
+}
+
+
+var root = document.createElement("div");
+document.body.appendChild(root);
+
 ReactDOM.render(
-  <p>Hello world</p>,
-  document.body
+    <div>
+        <Clock/>
+        <button onClick={clicked_me}>HeyJude</button>
+    </div>,
+    root
 );
